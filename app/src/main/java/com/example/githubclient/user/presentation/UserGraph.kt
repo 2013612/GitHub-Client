756d8b4a2a2c4447e3fce8aab415072ee03ea247
2013612 <samuelwong2013612@gmail.com>
2025-03-14 03:38:57 +0800

    feat: add userDetailScreen to userGraph

--- app/src/main/java/com/example/githubclient/user/presentation/UserGraph.kt
+++ app/src/main/java/com/example/githubclient/user/presentation/UserGraph.kt
@@ -3,6 +3,8 @@ package com.example.githubclient.user.presentation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.navigation
+import com.example.githubclient.user.presentation.detail.screen.NavUserDetailScreen
+import com.example.githubclient.user.presentation.detail.screen.userDetailScreen
 import com.example.githubclient.user.presentation.list.screen.NavUserListScreen
 import com.example.githubclient.user.presentation.list.screen.userListScreen
 import kotlinx.serialization.Serializable
@@ -13,7 +15,11 @@ object UserGraph
 fun NavGraphBuilder.userGraph(navController: NavController) {
     navigation<UserGraph>(startDestination = NavUserListScreen) {
         userListScreen(navigateToUserDetails = {
-            println(it)
+            navController.navigate(NavUserDetailScreen(it))
+        })
+
+        userDetailScreen(navigateBack = {
+            navController.navigateUp()
         })
     }
 }
