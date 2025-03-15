251a5fe185019f24fe2c05fabb6bf6db8946ab86
2013612 <samuelwong2013612@gmail.com>
2025-03-16 00:34:33 +0800

    feat: add UserDetailProfilePreview

--- app/src/main/java/com/example/githubclient/user/presentation/detail/component/UserDetailProfile.kt
+++ app/src/main/java/com/example/githubclient/user/presentation/detail/component/UserDetailProfile.kt
@@ -16,16 +16,25 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.ColorImage
+import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePreviewHandler
+import coil3.compose.LocalAsyncImagePreviewHandler
 import com.example.githubclient.R
+import com.example.githubclient.theme.GitHubClientTheme
 import com.example.githubclient.user.domain.model.UserProfile
 
 @Composable
@@ -119,6 +128,37 @@ private fun DetailRow(
             Image(painter = painter, contentDescription = null, modifier = Modifier.size(24.dp))
             Spacer(modifier = Modifier.width(8.dp))
             Text(text = text)
+        }
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Preview(backgroundColor = 0xFFFFFF)
+@Composable
+private fun UserDetailProfilePreview() {
+    val previewHandler =
+        AsyncImagePreviewHandler {
+            ColorImage(Color.Red.toArgb())
+        }
+
+    GitHubClientTheme {
+        CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler) {
+            UserDetailProfile(
+                profile =
+                    UserProfile(
+                        id = 1,
+                        userName = "John Doe",
+                        name = "John Doe",
+                        avatarUrl = "https://example.com/avatar.png",
+                        followers = 10,
+                        following = 20,
+                        company = "Example Inc.",
+                        location = "Exampleville",
+                        email = "james.monroe@examplepetstore.com",
+                        blog = "blog",
+                        twitterUsername = "twitterUsername",
+                    ),
+            )
         }
     }
 }
