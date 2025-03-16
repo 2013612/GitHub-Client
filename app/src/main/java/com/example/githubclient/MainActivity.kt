6c77f903e7a7b52113679566160d0425361c9c85
2013612 <samuelwong2013612@gmail.com>
2025-03-17 03:20:33 +0800

    fix: call installSplashScreen before enableEdgeToEdge

--- app/src/main/java/com/example/githubclient/MainActivity.kt
+++ app/src/main/java/com/example/githubclient/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.githubclient.theme.GitHubClientTheme
 import org.koin.androidx.compose.KoinAndroidContext
@@ -11,7 +12,11 @@ import org.koin.androidx.compose.KoinAndroidContext
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         enableEdgeToEdge()
+
         setContent {
             GitHubClientTheme {
                 KoinAndroidContext {
